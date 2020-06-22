5b8cd1f18ed6b74d9dca9bef4cc4cc77b6147f55
meansick93 <meansick93@gmail.com>
2020-06-22 16:34:42 -0400

    some practices

---
+++ practice/src/main/java/AllAboutMe.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author minsikkim
+ */
+public class AllAboutMe {
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        String favFood = "hot buttered gnocchi";
+        int numPets = 5;
+        String location = "pineapple under the sea";
+        boolean canWhistle = true;
+        String hobby = "whistle";
+        /*
+            My name is Ada Lovelace.
+            My favorite food is hot buttered gnocchi.
+            I have 5 pets.
+            I live in a pineapple under the sea, and I love it here.
+            It is true I know how to whistle.
+        */
+        System.out.println("My name is "+name+".");
+        System.out.println("My favorite foor is "+favFood+".");
+        System.out.println("I have "+numPets+" pets.");
+        System.out.println("I love in a "+location+", and I love it here");
+        System.out.println("It is "+canWhistle+" I know how to "+hobby);
+        
+    }
+}
